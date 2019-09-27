2b2131f5fddbdcb6b423c99cb1ac628f9d9f5916
Anthony Obando <obandoanthony.8@gmail.com>
2019-09-27 22:57:47 +0000

    Start of website

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # obandoAssignment2
+This website is meant to pull the most recent comic and a random comic.
---
+++ includes/config.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Used to store website configuration information.
+ *
+ * @var string or null
+ */
+function config($key = '')
+{
+    $config = [
+        'name' => 'Video Game History ',
+        'pretty_uri' => false,
+        'site_url' =>'https://anthony-obando-assignment-1.herokuapp.com/' , //'https://csuphpdemo.herokuapp.com'
+        'nav_menu' => [
+            '' => 'Overview',
+            'about-me' => 'About Me',
+            'contact'=> 'Contact',
+        ],
+        'template_path' => 'template',
+        'content_path' => 'content',
+        'version' => 'v1.0',
+    ];
+    return isset($config[$key]) ? $config[$key] : null;
+}
