f25615dd5cbd20296cb7a25bf8ca50bca13d6602
Anthony Obando <obandoanthony.8@gmail.com>
2019-09-28 21:13:49 +0000

    mods

--- template/comic.php
+++ template/comic.php
@@ -19,10 +19,7 @@
          <li class="nav-item"><a class="nav-link" tabindex="-1" href="https://anthony-obando-assignment2.herokuapp.com/">Home</a></li><li class="nav-item"><a class="nav-link" tabindex="-1" href="https://anthony-obando-assignment2.herokuapp.com/template/comic.php">Random Comic</a></li>        </ul>
       </div>
     </nav>
-<?php
-include 'content/home.phtml';
-include 'content/comic.phtml';
-?>
+   <p>Your random comic goes here.</p>
 <nav class="navbar fixed-bottom navbar-dark bg-dark">
       <a class="navbar-brand" href="#">Fixed bottom NavBar to get back to where you need</a>
     </nav>
