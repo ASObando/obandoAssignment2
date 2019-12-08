9de6cb0011de924baa1f56071e1ae71162cb5961
Anthony Obando <obandoanthony.8@gmail.com>
2019-12-08 00:36:45 +0000

    Change functions

--- includes/functions.php
+++ includes/functions.php
@@ -52,12 +52,11 @@ function page_title()
 function page_content()
 {
     $page = isset($_GET['page']) ? $_GET['page'] : 'home';
-    $path = getcwd() . '/' . config('content_path') . '/' . $page . '.php';
+    $path = getcwd() . '/' . config('content_path') . '/' . $page . '.phtml';
     if (! file_exists($path)) {
-        $path = getcwd() . '/' . config('content_path') . '/404.php';
-    }/*
-    echo file_get_contents($path);*/
-    require config('config_path'). $path;
+        $path = getcwd() . '/' . config('content_path') . '/404.phtml';
+    }
+    echo file_get_contents($path);
 }
 /**
  * Starts everything and displays the template.
